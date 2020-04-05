9b1ffdaed2797f5981d15dee3ea54788b74d3e74
Kyle Valdillez <kyle.valdillez@infor.com>
2020-04-04 21:37:43 -0400

    Added shop page with collection preview

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
-import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
+import "./App.css";
 import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
 
 function App() {
-    return <HomePage />;
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/shop" component={ShopPage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import "./menu-item.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({
+    title,
+    imageUrl,
+    size = "small",
+    history,
+    linkUrl,
+    match,
+}) => (
+    <div
+        className={`${size} menu-item`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
         <div
             style={{
                 backgroundImage: `url(${imageUrl})`,
@@ -17,4 +28,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
