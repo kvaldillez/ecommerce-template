5504d899edc6af465120e6046b9f336b59b500aa
Kyle Valdillez <kyle.valdillez@infor.com>
2020-04-12 20:24:49 -0400

    Implemented firebase utilities to store users in DB. Added sign up component

--- src/pages/sign-in-register/sign-in-register.js
+++ src/pages/sign-in-register/sign-in-register.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import SignIn from "../../components/sign-in/sign-in";
+import SignUp from "../../components/sign-up/sign-up";
 import "./sign-in-register.scss";
 
 const SignInRegisterPage = () => (
     <div className="sign-in-register">
         <SignIn />
+        <SignUp />
     </div>
 );
 
