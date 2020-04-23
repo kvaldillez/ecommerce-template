879f3974d618fbda047387bfb7d1081b63ec881a
Kyle Valdillez <kyle.valdillez@infor.com>
2020-04-22 22:58:18 -0400

    Added 'add to cart' button to collection preview and created 'addItem' action

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,9 +2,16 @@ import React from "react";
 
 import "./button.scss";
 
-const Button = ({ children, isGoogleSignIn, ...otherButtonProps }) => (
+const Button = ({
+    children,
+    isGoogleSignIn,
+    inverted,
+    ...otherButtonProps
+}) => (
     <button
-        className={`${isGoogleSignIn ? "google-sign-in" : ""} btn`}
+        className={`${inverted ? "inverted " : ""}${
+            isGoogleSignIn ? "google-sign-in " : ""
+        }btn`}
         {...otherButtonProps}
     >
         {children}
