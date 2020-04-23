212b0adc3858831f6634e61b2eb703df86b7837c
Kyle Valdillez <kyle.valdillez@infor.com>
2020-04-22 22:17:35 -0400

    Added cart icon, cart dropdown, and cart reducer for toggling dropdown

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { auth } from "../../firebase/firebase-utils";
+import CartIcon from "../cart-icon/cart-icon";
+import CartDropdown from "../cart-dropdown/cart-dropdown";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 import "./header.scss";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <header className="header">
         <Link to="/" className="logo-container">
             <Logo className="logo" />
@@ -26,12 +28,15 @@ const Header = ({ currentUser }) => (
                     Sign In
                 </Link>
             )}
+            <CartIcon />
         </nav>
+        {hidden ? null : <CartDropdown />}
     </header>
 );
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden,
 });
 
 export default connect(mapStateToProps)(Header);
