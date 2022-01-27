ae51535581d3c1743b31c5e59042207ff244b8b9
Kyle Valdillez <kyle.valdillez@gmail.com>
2022-01-27 01:02:37 -0500

    Added lazy loading components with spinner and an error boundary

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, lazy, Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { selectCurrentUser } from "./redux/user/user-selectors";
 import { checkUserSession } from "./redux/user/user-actions";
 
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import CheckoutPage from "./pages/checkout/checkout.component";
-import SignInRegisterPage from "./pages/sign-in-register/sign-in-register";
-
 import Header from "./components/header/header.component";
+import Spinner from "./components/spinner/spinner.component";
+import ErrorBoundary from "./components/error-boundary/error-boundary.component";
 import "./App.scss";
+
+const HomePage = lazy(() => import("./pages/homepage/homepage.component"));
+const ShopPage = lazy(() => import("./pages/shop/shop.component"));
+const CheckoutPage = lazy(() => import("./pages/checkout/checkout.component"));
+const SignInRegisterPage = lazy(() => import("./pages/sign-in-register/sign-in-register"));
 
 const App = () => {
     const currentUser = useSelector(selectCurrentUser);
@@ -25,24 +27,28 @@ const App = () => {
             <Header />
             <main className="page-container">
                 <Switch>
-                    <Route exact path="/" component={HomePage} />
-                    <Route path="/shop" component={ShopPage} />
-                    <Route
-                        exact
-                        path="/checkout"
-                        component={CheckoutPage}
-                    />
-                    <Route
-                        exact
-                        path="/sign-in"
-                        render={() =>
-                            currentUser ? (
-                                <Redirect to="/" />
-                            ) : (
-                                <SignInRegisterPage />
-                            )
-                        }
-                    />
+                    <ErrorBoundary>
+                        <Suspense fallback={<Spinner />}>
+                            <Route exact path="/" component={HomePage} />
+                            <Route path="/shop" component={ShopPage} />
+                            <Route
+                                exact
+                                path="/checkout"
+                                component={CheckoutPage}
+                            />
+                            <Route
+                                exact
+                                path="/sign-in"
+                                render={() =>
+                                    currentUser ? (
+                                        <Redirect to="/" />
+                                    ) : (
+                                        <SignInRegisterPage />
+                                    )
+                                }
+                            />
+                        </Suspense>
+                    </ErrorBoundary>
                 </Switch>
             </main>
         </Fragment>
---
+++ src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import './error-boundary.scss';
+
+class ErrorBoundary extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            hasErrored: false
+        };
+    }
+
+    static getDerivedStateFromError(error) {
+        return { hasErrored: true };
+    }
+
+    componentDidCatch(error, info) {
+        console.log(error);
+    }
+
+    render() {
+        if (this.state.hasErrored) {
+            return (
+                <div className="error-image-overlay">
+                    <div className="error-image-container" style={{ backgroundImage: 'url("https://i.imgur.com/yW2W9SC.png")'}} />
+                    <h2 className="error-image-text">Sorry this page is broken</h2>
+                </div>
+            )
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
